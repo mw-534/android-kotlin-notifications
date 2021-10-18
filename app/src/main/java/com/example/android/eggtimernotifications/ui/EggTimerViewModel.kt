85942da04314f65a2e7f5b18ae2ee323c5257339
ASUS <m>
2021-10-18 07:35:49 +0200

    Implemented using AlarmManager and a BroadcastReceiver to send the Notification when the timer has elapsed.

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerViewModel.kt
@@ -119,11 +119,11 @@ class EggTimerViewModel(private val app: Application) : AndroidViewModel(app) {
                 val triggerTime = SystemClock.elapsedRealtime() + selectedInterval
 
                 // Step 1.5 get an instance of NotificationManager and call sendNotification
-                val notifiactionManager = ContextCompat.getSystemService(
-                    app,
-                    NotificationManager::class.java
-                ) as NotificationManager
-                notifiactionManager.sendNotification(app.getString(R.string.timer_running), app)
+//                val notifiactionManager = ContextCompat.getSystemService(
+//                    app,
+//                    NotificationManager::class.java
+//                ) as NotificationManager
+//                notifiactionManager.sendNotification(app.getString(R.string.timer_running), app)
 
                 // TODO: Step 1.15 call cancel notification
 
