e1ef2c4553bcec713200840d632b2345b0f86cb8
ASUS <m>
2021-10-25 06:42:56 +0200

    Added canceling old notifications when the timer is started.

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -86,4 +86,10 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
 }
 
-// TODO: Step 1.14 Cancel all notifications
+// Step 1.14 Cancel all notifications
+/**
+ * Cancels all notifications.
+ */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
