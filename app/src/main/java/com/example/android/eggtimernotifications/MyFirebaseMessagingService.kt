2d4249fa0e6d1cc1728884c3d848d33d3d83bcb1
ASUS <m>
2021-11-01 14:36:05 +0100

    Implemented receiving data payload with FCM messaging.

--- app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -15,17 +15,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
-    override fun onMessageReceived(p0: RemoteMessage) {
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: ${p0.from}")
+        Log.d(TAG, "From: ${remoteMessage?.from}")
 
-        // TODO Step 3.5 check messages for data
+        // Step 3.5 check messages for data
         // Check if message contains a data payload.
-        Log.d(TAG, "Message Data Payload: ${p0.data}")
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message Data Payload: ${remoteMessage.data}")
+        }
 
-        // TODO Step 3.6 check messages for notification and call sendNotification
+        // Step 3.6 check messages for notification and call sendNotification
         // Check if message contains a notification payload.
-        p0.notification?.let {
+        remoteMessage?.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
             sendNotification(it.body!!)
         }
